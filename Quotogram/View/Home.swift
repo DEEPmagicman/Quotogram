6b778a2cda3d09daf9c6e428ca3ebcc97c4ee811
Deep <deepgandhi999@gmail.com>
2022-03-23 23:43:01 +0530

    add: sample lines

--- Quotogram/View/Home.swift
+++ Quotogram/View/Home.swift
@@ -12,9 +12,9 @@ struct Home: View {
     @State var cards: [Card] = [
         Card(cardColor: .blue, date: "Hirunaka no Ryuusei", title: "Thinking about whether it\'s because of jealousy or just saying it out loud by accident, aren\'t they all part of love? Love isn\'t always sweet."),
         Card(cardColor: .red, date: "Monday 8th November", title: "hello there is new"),
-        Card(cardColor: .yellow, date: "Monday 8th November", title: "hello there is new"),
-        Card(cardColor: .brown, date: "Monday 8th November", title: "hello there is new"),
-        Card(cardColor: .gray, date: "Monday 8th November", title: "hello there is new")
+        Card(cardColor: .yellow, date: "Amelie Earhart", title: "The most beautiful thing is the decision to act."),
+        Card(cardColor: .brown, date: "Leonardo", title: "Let us not take this planet for granted, I do not take tonight for granted"),
+        Card(cardColor: .gray, date: "Johnny Deep", title: "Awards are not important to me when I see a 10 year old boy saying I love Captain Jack Sparow")
     ]
     
     @State var showDetailPage: Bool = false
