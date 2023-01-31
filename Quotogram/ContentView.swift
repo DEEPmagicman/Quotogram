bde052880a9d6cff461a9907b8a91f03d37208c9
Deep <deepgandhi999@gmail.com>
2023-01-31 18:03:33 +0530

    check commit (just backup)

--- Quotogram/ContentView.swift
+++ Quotogram/ContentView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var abcd = Keyur_CarbonIntensity()
+    
     var body: some View {
         Home()
+            .onAppear {
+                
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class Keyur_CarbonIntensity: ObservableObject{
+    @Published var shortname: String = String()
+    @Published var forecast: String = String()
+//    func getData() {
+    init () {
+        let jsonUrlString = "https://api.carbonintensity.org.uk/regional/postcode/SE7"
+        
+        guard let url = URL(string: jsonUrlString) else{
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, res, err) in
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                print(json)
+            } catch {
+                print("didnt work")
+            }
+        }.resume()
+        
+    }
+    
+    func getRequired(json: [String: Any]?) {
+//        let json = json
+//        ((json?["data"] as? NSArray)?.firstObject as? NSDictionary)?["shortname"]
+//        (((((json?["data"] as? NSArray)?.firstObject as? NSDictionary)?["data"] as? NSArray)?.firstObject as? NSDictionary)?["intensity"] as? NSDictionary)?["forecast"]
+        if let json = json, let data = json["data"] as? [[String: Any]], let firstData = data.first,   let shortName = firstData["shortname"] as? String {
+            print(shortName)
+        }
     }
 }
--- Quotogram/Model/Card.swift
+++ Quotogram/Model/Card.swift
@@ -10,7 +10,24 @@ import SwiftUI
 struct Card: Identifiable {
     
     var id = UUID().uuidString
-    var cardColor: Color
+    var cardColor: Color = Color.randomColor()
     var date: String = ""
     var title: String
+}
+
+extension Color {
+    static func randomColor() -> Color {
+        switch Int.random(in: 0...3) {
+        case 0:
+            return Color(red: 0.26, green: 133/255, blue: 244/255)
+        case 1:
+            return Color(red: 219/255, green: 68/255, blue: 55/255)
+        case 2:
+            return Color(red: 244/255, green: 180/255, blue: 0)
+        case 3:
+            return Color(red: 15.255, green: 157/255, blue: 88/255)
+        default:
+            return .black
+        }
+    }
 }
